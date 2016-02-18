688177714bf7cb6053c82a9319e9a9b75aa5e18a
TK <tarahek@yahoo.ca>
2016-02-17 23:02:37 -0500

    updates

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+  // var $window = $(window),
+  //      $stickyEl = $('#the-sticky-div'),
+  //      elTop = $stickyEl.offset().top;
+
+  //  $window.scroll(function() {
+  //       $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+  //   });
+
+  
